67dd686fa5121da83f8559146beaa5c838b02300
ColinChester <colin.chester@bison.howard.edu>
2025-03-20 19:23:57 -0400

    started problem 2

---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
@@ -0,0 +1,71 @@
+package org.howard.edu.lsp.midterm.question2;
+
+public class Book {
+	String title;
+	String author;
+	String isbn;
+	int pYear;
+	
+	
+
+	public Book(String title, String author, String isbn, int pYear) {
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.pYear = pYear;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getpYear() {
+		return pYear;
+	}
+
+	public void setpYear(int pYear) {
+		this.pYear = pYear;
+	}
+	
+	/**
+	 * This method compares the author and ISBN of two books to see if they are equal
+	 * 
+	 * @param b is the book object that the caller is being compared to
+	 * @return True if the books have the same ISBN and author, and false otherwise
+	 */
+	public boolean equals(Object b) {
+		if (this.author.equals(b.author) && this.isbn.equals(b.isbn)) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * This method returns a String representation of 
+	 */
+	@Override
+	public String toString() {
+		return "Title: " + this.title + ", Author: " + this.author + ", ISBN: " + this.isbn + ", Year Published: " + this.pYear;
+	}
+	
+}
