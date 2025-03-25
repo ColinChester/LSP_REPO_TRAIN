5fdd3ecc245a2e19e75749e130b2b55c63bbc73d
ColinChester <colin.chester@bison.howard.edu>
2025-03-25 14:22:39 -0400

    Completed Assignment 5
    IntegerSet Javadoc is located in the mirrored package path within org/howard/edu/lsp/assignment5/doc

--- Spring_2025/src/org/howard/edu/lsp/assignment5/Driver.java
+++ Spring_2025/src/org/howard/edu/lsp/assignment5/Driver.java
@@ -1,5 +1,66 @@
-package assignment5;
+package org.howard.edu.lsp.assignment5;
+
+import java.util.ArrayList;
 
 public class Driver {
-
+	public static void main(String[] args) {
+		IntegerSet set1 = new IntegerSet();
+		String test1 = "";
+		String test2 = null;
+	    set1.add(1);
+	    set1.add(2);
+	    set1.add(3);
+	    set1.add(4);
+	    set1.add(5);
+	    System.out.println("Set1 values: " + set1);
+	    System.out.println("Set1 length: " + set1.length());
+	    System.out.println("Set1 contains 9: " + set1.contains(9));
+	    System.out.println("Adding 9 to set1...");
+	    set1.add(9);
+	    System.out.println("Adding 1 to set1...");
+	    set1.add(1);
+	    System.out.println("Set1 values: " + set1);
+	    System.out.println("Set1 contains 9: " + set1.contains(9));
+	    System.out.println("Removing 9 from set1...");
+	    set1.remove(9);
+	    System.out.println("Removing 10 from set1");
+	    set1.remove(10);
+	    System.out.println("Set1 values: " + set1);
+	    System.out.println("Smallest: " + set1.smallest());
+	    System.out.println("Largest: " + set1.largest());
+	    System.out.println("Set1 equals set1: " + set1.equals(set1));
+	    System.out.println("Set1 equals string: " + set1.equals(test1));
+	    System.out.println("Set1 equals null: " + set1.equals(test2));
+	    IntegerSet set2 = new IntegerSet();
+	    set2.add(4);
+	    set2.add(5);
+	    set2.add(6);
+	    set2.add(7);
+	    set2.add(8);
+	    System.out.println("Set2 values: " + set2);
+	    System.out.println("Set1 equals set2: " + set1.equals(set2));
+	    System.out.println("Set1 values: " + set1);
+	    System.out.println("Set2 values: " + set2);
+        IntegerSet unionTest = new IntegerSet(new ArrayList<>(set1.getSet()));
+        unionTest.union(set2);
+        System.out.println("After union, set1: " + unionTest);
+        IntegerSet intersectTest = new IntegerSet(new ArrayList<>(set1.getSet()));
+        intersectTest.intersect(set2);
+        System.out.println("After intersection, set1: " + intersectTest);
+        IntegerSet diffTest = new IntegerSet(new ArrayList<>(set1.getSet()));
+        diffTest.diff(set2);
+        System.out.println("After difference, set1: " + diffTest);
+        IntegerSet complementTest = new IntegerSet(new ArrayList<>(set1.getSet()));
+        complementTest.complement(set2);
+        System.out.println("After complement, set1: " + complementTest);
+        System.out.println("Set1 values: " + set1);
+        System.out.println("Set1 is empty: " + set1.isEmpty());
+        System.out.println("Clearing set1");
+        set1.clear();
+        System.out.println("Set1 values: " + set1);
+        System.out.println("Set1 is empty: " + set1.isEmpty());
+    }
+	    
+	    
+	    
 }
--- Spring_2025/src/org/howard/edu/lsp/assignment5/IntegerSet.java
+++ Spring_2025/src/org/howard/edu/lsp/assignment5/IntegerSet.java
@@ -1,67 +1,188 @@
-package assignment5;
+package org.howard.edu.lsp.assignment5;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+
 
 public class IntegerSet {
-	// TODO Store the set elements in an ArrayList.
 	private List<Integer> set = new ArrayList<Integer>();
 
-	// TODO Default Constructor
+	/**
+	 * Default IntegerSet constructor
+	 */
 	public IntegerSet() {
 		
 	}
-
-	// TODO Constructor if you want to pass in already initialized
+	
+	/**
+	 * IntegerSet constructor with a passed set
+	 * @param set	Integer ArrayList to be passed into this.set
+	 */
 	public IntegerSet(ArrayList<Integer> set) {
 			this.set = set;
 	}
-
-	// TODO Clears the internal representation of the set. 5 pts.
-	public void clear() {…};
-
-	// TODO Returns the length of the set. 5 pts.
-	public int length() {…};
-
-	/* TODO
-	 * Returns true if the 2 sets are equal, false otherwise;
-	 * Two sets are equal if they contain all of the same values in ANY order.  This overrides
-	 * the equal method from the Object class. 10 pts.
-	*/
-	public boolean equals(Object o) {…}; 
-
-	// TODO Returns true if the set contains the value, otherwise false. 5 pts.
-	public boolean contains(int value) {…};
 	
-	// TODO Returns the largest item in the set. 5 pts.
-	public int largest()  {…};
+	/**
+	 * Clears this.set
+	 */
+	public void clear() {
+		this.set.clear();
+	};
+
+	/**
+	 * Returns the length of this.set
+	 * @return the int value of this.set's length
+	 */
+	public int length() {
+		return this.set.size();
+	};
+
+
+	/**
+	 * Determines if the two passed sets have the same values
+	 * Referenced: https://www.geeksforgeeks.org/java-program-to-compare-two-objects/
+	 * @return true if the object passed shares values with this.object
+	 * @return false if the object passed is null or not an IntegerSet object
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		
+		if (o == null || this.getClass() != o.getClass()) {
+			return false;
+		}
+		IntegerSet newSet = (IntegerSet)o;
+		return this.set.equals(newSet.set);
+	}; 
+
+	/**
+	 * Determines if the passed value is in this.set
+	 * @param value	int value to be found in this.set
+	 * @return true if value is in this.set
+	 * @return false if value is not in this.set
+	 */
+	public boolean contains(int value) {
+		return this.set.contains(value);
+	};
 	
-	// TODO Returns the smallest item in the set. 5 pts.
-	public int smallest()  {…};
+	
+	/**
+	 * Finds the largest value in this.set
+	 * References: https://stackoverflow.com/questions/12788182/finding-the-largest-value-in-an-arraylist
+	 * @return max	Largest value in this.set
+	 */
+	public int largest()  {
+		int max = Collections.max(this.set);
+		return max;
+	};
+	
+	/**
+	 * Finds the smallest value in this.set
+	 * @return min	Smallest value in this.set
+	 */
+	public int smallest()  {
+		int min = Collections.min(this.set);
+		return min;
+	};
 
-	// TODO Adds an item to the set or does nothing it already there. 5 pts.	
-	public void add(int item) {…};
+	
+	/**
+	 * Adds a value to this.set if the value is not already present
+	 * @param item	int value to add
+	 */
+	public void add(int item) {
+		if (!this.set.contains(item)) {
+			this.set.add(item);
+		}
+	};
 
-	// TODO Removes an item from the set or does nothing if not there. 5 pts.
-	public void remove(int item) {…}; 
+	/**
+	 * Removes a value from this.set if the value is present
+	 * @param item	int value to be removed
+	 */
+	public void remove(int item) {
+		if (this.set.contains(item)) {
+			int index = this.set.indexOf(item);
+			this.set.remove(index);
+		}
+	}; 
 
-	// TODO Set union. 11 pts.
-	public void union(IntegerSet intSetb) {…};
+	/**
+	 * Updates this.set to be the union of itself and the set of the passed IntegerSet, the union being the combination of the sets
+	 * References: https://stackoverflow.com/questions/9917787/merging-two-arraylists-into-a-new-arraylist-with-no-duplicates-and-in-order-in
+	 * @param intSetb	IntegerList to be added
+	 */
+	public void union(IntegerSet intSetb) {
+		this.set.removeAll(intSetb.set);
+		this.set.addAll(intSetb.set);
+	};
 
-	// TODO Set intersection, all elements in s1 and s2. 12 pts.
-	public void intersect(IntegerSet intSetb) {…}; 
+	
+	/**
+	 * Updates this.set to be the intersection of itself and the set of the passed IntegerSet, the intersection being all integers that are shared between the sets
+	 * References: https://stackoverflow.com/questions/5283047/intersection-and-union-of-arraylists-in-java
+	 * @param intSetb	IntegerSet to be compared
+	 */
+	public void intersect(IntegerSet intSetb) {
+		var newList = new ArrayList<Integer>();
+		for (int i : this.set) {
+			if(intSetb.contains(i)) {
+				newList.add(i);
+			}
+		}
+		this.set = newList;
+	}; 
 
-	// TODO Set difference, i.e., s1 – s2. 12 pts.
-	public void diff(IntegerSet intSetb);
+	/**
+	 * Updates this.set to be the difference between itself and the set of the passed IntegerSet, the difference being the values in this.set that are not in the passed set
+	 * @param intSetb	IntegerSet to be compared
+	 */
+	public void diff(IntegerSet intSetb) {
+		for (int i : intSetb.set) {
+			if (this.set.contains(i)) {
+				this.set.remove(this.set.indexOf(i));
+			}
+		}
+	};
 
-	// TODO Set complement, all elements not in s1. 11 pts.
-	public void complement(IntegerSet intSetb) {…}
+	/**
+	 * Updates this.set to be the complement of itself and the set of the passed IntegerSet, the complement being the values of the passed set not in this.set
+	 * @param intSetb	IntegerSet to be compared
+	 */
+	public void complement(IntegerSet intSetb) {
+		var newList = new ArrayList<>(intSetb.set);
+		newList.removeAll(this.set);
+		this.set = newList;
+	}
 
-	// TODO Returns true if the set is empty, false otherwise. 5 pts.
-	boolean isEmpty(); 
+	/**
+	 * Determines whether this.set is empty
+	 * @return true if this.set is empty
+	 * @return false if this.set is not empty
+	 */
+	boolean isEmpty() {
+		return this.set.isEmpty();
+	}; 
+	
+	/**
+	 * Getter for this.set
+	 * @return	this.set
+	 */
+	public List<Integer> getSet(){
+		return this.set;
+	}
 
-	// TODO Return String representation of your set.  This overrides the equal method from the Object class. 5 pts.
+
+	/**
+	 * String representation of the IntegerSet object
+	 * @return	set.toString()
+	 */
+	@Override
 	public String toString() {
-		set.toString();
+		return set.toString();
 		};
 
 }
